16f93768cb6bf7b24e07174a87b7080af458172e
AntonioMendez034 <A01738269@tec.mx>
2026-04-21 16:43:16 -0600

    Add files via upload

---
+++ evaluacion_4_2.m
@@ -0,0 +1,109 @@
+clear
+close all
+clc
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TIEMPO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+tf = 2*pi;             
+ts = 0.1;            
+t = 0: ts: tf;       
+N = length(t);       
+
+%%%%%%%%%%%%%%%%%%%%%%%% CONDICIONES INICIALES %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+x1 = zeros(1,N+1);  
+y1 = zeros(1,N+1);  
+phi = zeros(1,N+1); 
+
+x1(1) = 4;              % inicio en el círculo
+y1(1) = 0;              
+phi(1) = 0;             
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% PUNTO DE CONTROL %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+hx = zeros(1,N+1);  
+hy = zeros(1,N+1);  
+
+hx(1) = x1(1);
+hy(1) = y1(1);
+
+%%%%%%%%%%%%%%%%%%%%%% VELOCIDADES DE REFERENCIA %%%%%%%%%%%%%%%%%%%%%%%%%%
+u = zeros(1,N); 
+w = zeros(1,N); 
+
+for k = 1:N
+    
+    tk = t(k);
+    
+    dx = -4 * sin(tk);
+    dy = 4 * cos(tk);
+    
+    ddx = -4 * cos(tk);
+    ddy = -4 * sin(tk);
+    
+    u(k) = sqrt(dx^2 + dy^2);
+    w(k) = (dx * ddy - dy * ddx) / (dx^2 + dy^2);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%% BUCLE DE SIMULACION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for k=1:N 
+    
+    tk = t(k);
+    
+    dx = -4 * sin(tk);
+    dy = 4 * cos(tk);
+    
+    phi(k+1) = atan2(dy, dx);
+    
+    xp1 = u(k)*cos(phi(k+1)); 
+    yp1 = u(k)*sin(phi(k+1));
+
+    x1(k+1)=x1(k) + xp1*ts; 
+    y1(k+1)=y1(k) + yp1*ts; 
+    
+    hx(k+1)=x1(k+1); 
+    hy(k+1)=y1(k+1);
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SIMULACION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+scene=figure;
+set(scene,'Color','white');
+axis equal;
+grid on;
+box on;
+xlabel('x(m)'); ylabel('y(m)');
+
+view(2);
+axis([-5 5 -5 5]);   % círculo centrado
+
+scale = 2;
+MobileRobot_5;
+H1=MobilePlot_4(x1(1),y1(1),phi(1),scale);hold on;
+
+H2=plot(hx(1),hy(1),'r','lineWidth',2);
+
+for k=1:N
+
+    delete(H1);    
+    delete(H2);
+    
+    H1=MobilePlot_4(x1(k),y1(k),phi(k),scale);
+    H2=plot(hx(1:k),hy(1:k),'r','lineWidth',2);
+    
+    pause(ts);
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% GRAFICAS %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+figure;
+
+subplot(211)
+plot(t,u,'b','LineWidth',2),grid on
+xlabel('Tiempo [s]'),ylabel('m/s'),legend('u')
+
+subplot(212)
+plot(t,w,'r','LineWidth',2),grid on
+xlabel('Tiempo [s]'),ylabel('[rad/s]'),legend('w')
